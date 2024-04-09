1131824908a7840b3849217f250ff9d5a4987a57
Chad Bailey <chadbailey@gmail.com>
2024-04-09 15:33:01 +0000

    fixup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://daily-co.github.io/dailyai-docs",
+  url: "https://daily-co.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/dailyai-docs/",
