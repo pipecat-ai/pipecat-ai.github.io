c63df6192fbb904bbaea4786872aee55f29f93e1
Chad Bailey <chadbailey@gmail.com>
2024-05-13 18:37:35 +0000

    updated favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,7 @@ const config = {
 
   onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
